44bd940d4bf708bb113e353f26f115559bc3f4ba
Aniq55 <aniqrah@gmail.com>
2025-12-15 13:39:31 +0000

    latest for causal

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 # datasets=("uci")
 # datasets=("lastfm" "mooc")
-datasets=("ctig_test")
+datasets=("ctig_0")
 
 for dataset in "${datasets[@]}" 
 do
@@ -17,6 +17,8 @@ do
         python train_self_supervised.py --n_epoch 100 --data "$dataset" --n_runs 1 --use_memory --memory_updater rnn --embedding_module time --prefix jodie_rnn --modelname JODIE --patience 10
     done
 
+    for iter in {1..10}; do
+
     for sample in {1..10}; do
         distort="shuffle_${sample}_"
         python train_self_supervised.py --n_epoch 100  --distortion "$distort" --prefix tgn-attn --data "$dataset" --n_runs 1 --modelname TGN --patience 10
@@ -25,6 +27,8 @@ do
     for sample in {1..10}; do
         distort="shuffle_${sample}_"
         python train_self_supervised.py --n_epoch 100 --distortion "$distort" --data "$dataset" --n_runs 1  --use_memory --memory_updater rnn --embedding_module time --prefix jodie_rnn --modelname JODIE --patience 10
+    done
+
     done
 
     # # distorted data: all samples
